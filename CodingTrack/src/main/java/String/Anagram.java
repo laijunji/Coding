b42baa4e45c87fae3f2c053ecabe3d9f3843a7f7
junji lai <33259190+laijunji@users.noreply.github.com>
2025-10-12 14:58:20 +0800

    add new solution

--- CodingTrack/src/main/java/String/Anagram.java
+++ CodingTrack/src/main/java/String/Anagram.java
@@ -12,10 +12,27 @@ public class Anagram {
         return Arrays.equals(charArrayOne,charArrayTwo);
 
     }
+    
+    public boolean permutation(String s1, String s2) {
+        char[] chars = new char[128];
+        char[] chars1 = s1.toCharArray();
+        for (char c : chars1) {
+            chars[c]++;
+        }
+
+        for (int j = 0; j < s2.length(); j++) {
+            chars[s2.charAt(j)]--;
+            if(chars[s2.charAt(j)] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Anagram anagram = new Anagram();
         System.out.println(anagram.isAnagram("abc", "bac"));
+        System.out.println(anagram.permutation("abc", "bac"));
 
     }
 }
